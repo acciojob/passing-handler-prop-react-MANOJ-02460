0ded5220fb8affc84edaa360186d59911843abea
MANOJ <rajarapumanoj1999@gmail.com>
2026-09-21 14:51:32 +0530

    Implement handler prop color selection

--- src/components/App.js
+++ src/components/App.js
@@ -34,7 +34,7 @@ const App = () => {
     <div id="master">
       <h5 className="heading">{title}</h5>
 
-      <div className="row">
+      <div className="row children-wrapper" >
         {colourConfig.map((config) => (
           <ColourSelector
             key={config.key}
