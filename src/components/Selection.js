fd0697f89ef2c4088b7202933dca540c98f176d1
MANOJ <rajarapumanoj1999@gmail.com>
2026-09-24 20:12:22 +0530

    Implemented nextStyle

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import '../styles/Child.css';
 
-const Selection = ({applyColor}) => {
-    const [style, setStyle] = useState({background:""})
+const Selection = ({ applyColor }) => {
+    const [style, setStyle] = useState({ background: "" })
     return (
         <div className="fix-box"
-        data-testid="selection-box"
-        style={style}
-        onClick={()=>applyColor(setStyle)}
+            data-testid="selection-box"
+            style={style}
+            onClick={() => applyColor(nextStyle => setStyle(nextStyle))}
         >
             Selection
         </div>
